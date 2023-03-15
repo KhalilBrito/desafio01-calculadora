888d1d5eb0b4e900463b4bfa20f3f6db560f705c
Khalil BM <khalil.bm@hotmail.com>
2023-03-15 02:03:02 -0300

    Novos Buttons

--- src/App.js
+++ src/App.js
@@ -94,30 +94,32 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
+          <Button label="+" onClick={handleSumNumbers}/>  
+          <Button label="-" onClick={handleMinusNumbers}/>
           <Button label="x" onClick={handleMultNumbers}/>
-          <Button label="/" onClick={handleDivideNumbers}/>
-          <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="÷" onClick={handleDivideNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button class="zero" label="0" onClick={() => handleAddNumber('0')} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="." onClick={() => handleAddNumber('.')}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
+          <Button label="C" onClick={handleOnClear}/>
         </Row>
-        
+        <Row>
+          <Button label="=" class="equal" onClick={handleEquals}/>
+        </Row>
       </Content>
     </Container>
   );
